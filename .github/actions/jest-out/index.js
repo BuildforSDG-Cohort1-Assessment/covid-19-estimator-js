5b60ffe37a20d1d5032618d74165a878df3e231f
Charles Odili <chaluwa@gmail.com>
2020-04-05 16:11:58 +0100

    ready for tpl trial

--- .github/actions/jest-out/index.js
+++ .github/actions/jest-out/index.js
@@ -1,10 +1,22 @@
+const axios = require('axios');
 const core = require('@actions/core');
 const { context } = require("@actions/github");
-const axios = require('axios');
-const stats = require('../../../audits/ch-2.json');
+
+const CHALLENGES = {
+  'ch-1': 'challenge-01',
+  'ch-2': 'challenge-02',
+  'ch-3': 'challenge-03',
+  'ch-4': 'challenge-04',
+  'ch-5': 'challenge-05'
+};
 
 const run = async () => {
     try {     
+        const task = core.getInput('challenge');
+        const language = core.getInput('lang');
+        const challenge = CHALLENGES[task];
+        const stats = require(`../../../audits/${task}.json`);
+      
         const repo = context.repo.repo;
         const owner = context.repo.owner;
         
@@ -17,13 +29,16 @@ const run = async () => {
         } = stats;
 
         const report = {
-          repo, owner,
+          repo, 
+          owner,
+          language,
           numTotalTests, 
-          numPassedTests, 
+          source: 'jest',
+          numPassedTests,
+          type: challenge,
           numPendingTests,
           numTotalTestSuites, 
-          numPassedTestSuites,
-          type: 'challenge-02'
+          numPassedTestSuites
         };
 
         if(stats.testResults) {
@@ -34,9 +49,8 @@ const run = async () => {
           }
         }
 
-        const apiBase = core.getInput('api-base');
-        const { result } = await axios.post(`${apiBase}/entry-tests`, {report});
-        core.info(result);
+        const server = core.getInput('server');
+        await axios.post(`${server}/entry-tests`, {report});
 
       } catch (error) {
         core.setFailed(error.message);
--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 
 > Build an overly simplified COVID-19 infection impact estimator
 
-This is an eligibility assessment for the [#BuildforSDG](https://buildforsdg.andela.com/) program
+This is an eligibility assessment for the 2020 [#BuildforSDG](https://buildforsdg.andela.com/) program
 
 The assessment empowers me to **attempt** helping society and leaders prepare for the **real rig problem** of COVID-19, which is **its impact on lives, health systems, supply chains, and the economy**: 
 > 1.  Too many patients, not enough hospitals and beds. A serious shortage of ventilators, masks and other PPE - if *we don’t practice social distancing*.
 > 2.  Job losses or freezes, low cash flow and low production (even for essentials like food). These and more from too many people being sick, a sizable number dying (including some of the best people in many fields), and many others affected by the impact of losing loved ones or a world operating in slow motion
 
-## Project Setup & Submission Process
+## How To Proceed
+
+### Project Setup & Submission Process
 
 ---
-> Go here for a [guide and step-by-step video](https://drive.google.com/drive/u/0/folders/132af5VHpYX5LDTzqQETThXpDpw6Q6jRv) on the project setup and submission process
+> Go to [this Google Drive](https://drive.google.com/drive/u/0/folders/132af5VHpYX5LDTzqQETThXpDpw6Q6jRv) for guides on how to setup your project, take the assessment and submit your work. Make sure you read the instructions carefully, because missing a step might cost you in the long run.
 ---
 
+
+### Ethics & Honor Code
 
 
