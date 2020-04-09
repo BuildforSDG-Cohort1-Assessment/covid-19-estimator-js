17b4533a079c67b585caacf744e0de8757a11618
bekeplar <bekeplar@gmail.com>
2020-04-09 03:05:19 +0300

    Add impact estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  
+  return {
+    data:{
+      reportedCases;
+    },
+    impact: { // simple case scenario.
+      currentlyInfected: reportedCases * 10,
+      infectionsByRequestedTime: currentlyInfected * 1024
+    },
+    severeImpact: { // worst case scenario.
+      currentlyInfected: reportedCases * 50,
+      infectionsByRequestedTime: currentlyInfected * 1024
+    }
+  };;
+};
 
 export default covid19ImpactEstimator;
